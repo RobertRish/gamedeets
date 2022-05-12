eeb36242f7fb3d4afb65fca066e8499ffae8269f
Robby Rish <robbyrish13@gmail.com>
2022-05-12 12:07:25 -0400

    Updates

---
+++ precache-manifest.d0127ccd1c1171cd651feac3c868e9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4c2c027dec4a3407f233e15f51c48f",
+    "url": "/gamedeets/index.html"
+  },
+  {
+    "revision": "ce1ea95aabf795365836",
+    "url": "/gamedeets/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "a1bc1c87d433ab85e088",
+    "url": "/gamedeets/static/css/main.c4767ca9.chunk.css"
+  },
+  {
+    "revision": "ce1ea95aabf795365836",
+    "url": "/gamedeets/static/js/2.7ea81c22.chunk.js"
+  },
+  {
+    "revision": "38979214c50e9af90599d3be5871eaa2",
+    "url": "/gamedeets/static/js/2.7ea81c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bc1c87d433ab85e088",
+    "url": "/gamedeets/static/js/main.3f4e4d99.chunk.js"
+  },
+  {
+    "revision": "0888ac71942a5a669a84",
+    "url": "/gamedeets/static/js/runtime-main.31473481.js"
+  }
+]);
